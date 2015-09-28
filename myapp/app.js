b6d68feacdab88280197a866668b435868f4e493
Juan Rivillas <jprivillaso@gmail.com>
2015-09-27 21:53:32 -0300

    Created free web service endpoint to authenticate and added token security to the other web services

--- myapp/app.js
+++ myapp/app.js
@@ -6,22 +6,23 @@ var cookieParser  = require('cookie-parser');
 var bodyParser    = require('body-parser');
 var jwt           = require('jsonwebtoken');
 
-var config = require("./config");
-
 // Database
 var mongo = require('mongodb');
 var monk = require('monk');
 var db = monk('localhost:27017/restfulNodeApp');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
-var auth = require('./routes/auth');
+var indexRoute = require('./routes/index'); 
+var usersRoute = require('./routes/users');
 
 var app = express();
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
+
+var config = require('./config'); // get our config file
+app.set('superSecret', config.secret); // secret variable
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -31,106 +32,105 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Make our db accessible to our router
-app.use(function(req,res,next){
-    req.db = db;
-    next();
+/* GET home page. */
+app.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
 });
 
-/* Authentication  */
+// ---------------------------------------------------------
+// get an instance of the router for api routes
+// ---------------------------------------------------------
+var apiRoutes = express.Router(); 
+
+// ---------------------------------------------------------
+// authentication (no middleware necessary since this isnt authenticated)
+// ---------------------------------------------------------
+// http://localhost:8080/api/authenticate
 app.post('/authenticate', function(req, res) {
 
-  var name = req.body.user;
-  var pass = req.body.password;
+  console.log("authenticating");
 
-  if (name === "juan" && pass === "123") {
+  var user = {
+    password: req.body.password
+  }
 
-    var token = jwt.sign("juan", "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJfaWQiOiI1NDY1MDViMDFmYTAzYmUwMTUxMDYwOWIiLCJuYW1lIjoiTmljayBDZXJtaW5hcmEiLCJwYXNzd29yZCI6InBhc3N3b3JkIiwiYWRtaW4iOnRydWUsIl9fdiI6MH0.ah-NFQ1967WVeN6lYNAahT7hZtshG6kw6AW3ncuJOYw", {
-      expiresInMinutes: 1440 // expires in 24 hours
-    });
+  if (!user) {
+    res.json({ success: false, message: 'Authentication failed. User not found.' });
+  } else if (user) {
 
-    app.set('superSecret', token); // sec
-    console.log("User authenticated successfully");
-    res.status(500).json({token: token});
+    // check if password matches
+    if (user.password != req.body.password) {
+      res.json({ success: false, message: 'Authentication failed. Wrong password.' });
+    } else {
 
-  } else {
+      // if user is found and password is right
+      // create a token
+      var token = jwt.sign(user, app.get('superSecret'), {
+        expiresInMinutes: 15 // expires in 24 hours
+      });
 
-    console.error("Invalid login data");
-    res.status(400).send("Invalid login data");
+      res.json({
+        success: true,
+        message: 'Enjoy your token!',
+        token: token
+      });
+    }   
 
   }
 
 });
 
-// app.use('/auth', auth);
+// Make our db accessible to our router
+/*app.use(function(req,res,next){
+    req.db = db;
+    next();
+});*/
 
-app.use(function(req, res, next) {
+
+// ---------------------------------------------------------
+// route middleware to authenticate and check token
+// ---------------------------------------------------------
+apiRoutes.use(function(req, res, next) {
 
   console.log("middleware say hi");
 
   // check header or url parameters or post parameters for token
-  var token = req.body.token || req.query.token || req.headers['x-access-token'];
+  var token = req.body.token || req.headers['x-access-token'];
 
   // decode token
   if (token) {
 
     // verifies secret and checks exp
-    jwt.verify(token, app.get('superSecret'), function(err, decoded) {
-
+    jwt.verify(token, app.get('superSecret'), function(err, decoded) {      
       if (err) {
-        return res.json({ success: false, message: 'Failed to authenticate token.' });
+        return res.json({ success: false, message: 'Failed to authenticate token.' });    
       } else {
         // if everything is good, save to request for use in other routes
-        req.decoded = decoded;
+        req.decoded = decoded;  
         next();
       }
     });
-
+ 
   } else {
 
     // if there is no token
     // return an error
-    return res.status(403).send({
-        success: false,
-        message: 'No token provided.'
+    return res.status(403).send({ 
+      success: false, 
+      message: 'No token provided.'
     });
-
+    
   }
+  
 });
 
-/*
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+// ---------------------------------------------------------
+// authenticated routes
+// ---------------------------------------------------------
+apiRoutes.get('/', function(req, res) {
+  res.json({ message: 'Welcome to the coolest API on earth!' });
 });
 
-// error handlers
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
-  });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
-});
-*/
-
-app.use('/', routes);
-app.use('/users', users);
-
+app.use('/users', usersRoute);
+app.use('/api', apiRoutes);
 module.exports = app;
