2f9483bf05cae5c4e76fe03b9baead7b1c188d47
Juan Rivillas <jprivillaso@gmail.com>
2015-09-29 23:53:41 -0300

    Deleted unnecessary files

--- myapp/routes/users.js
+++ myapp/routes/users.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var router = express.Router();
-var authentication = require("../app/authentication.js");
 
 /* GET users listing. */
 router.get('/userlist', function(req, res) {
